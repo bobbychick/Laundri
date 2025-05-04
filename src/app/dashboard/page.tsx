a75b190b6d84ca3aa5c2d73b492f3ca78556449f
James <James01lw1998@gmail.com>
2025-05-04 12:50:46 +1000

    redid service dist

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,4 @@
-import { ChartAreaInteractive } from "@/components/chart-area-interactive";
+import { ServiceDistribution } from "@/components/dashboard/service-distribution";
 import { SiteHeader } from "@/components/layout/site-header";
 import { SectionCards } from "@/components/section-cards";
 
@@ -10,7 +10,7 @@ export default function Home() {
         <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
           <SectionCards />
           <div className="px-4 lg:px-6">
-            <ChartAreaInteractive />
+            <ServiceDistribution />
           </div>
         </div>
       </div>
